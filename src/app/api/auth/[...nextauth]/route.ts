cada5b25393a09a5ab1cf15b78f3026b5770d577
yanfer <yanfer.daniel@gmail.com>
2023-11-24 12:09:08 -0600

    add secret key to route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,7 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
 });
 
 export { handler as GET, handler as POST };
